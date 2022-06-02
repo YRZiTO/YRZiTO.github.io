60a3ae927e461f2a52ea8bb2f86b18eeeb87ceb7
YRZiTO <YousifZiTO@gmail.com>
2022-06-02 12:12:33 -0400

    cleaned the code

--- js/theme.js
+++ js/theme.js
@@ -1,21 +1,7 @@
 const switchTheme = document.getElementById('switchTheme');
 
 switchTheme.addEventListener('click', function onClick(event) {
-    // const header = document.getElementById('header');
-    // const footer = document.getElementById('footer');
-    // if (header.style.backgroundColor === 'black' && footer.style.backgroundColor === 'black') {
-    //     header.style.backgroundColor = 'white';
-    //     header.style.color = 'black';
-    //     footer.style.backgroundColor = 'white';
-    //     footer.style.color = 'black';
-    // } else {
-    //     header.style.backgroundColor = 'black';
-    //     header.style.color = 'white';
-    //     footer.style.backgroundColor = 'black';
-    //     footer.style.color = 'white';
-    // }
     const body = document.getElementById('body');
-    const bodyResume = document.getElementById('bodyResume');
     const headline = document.getElementById('headline');
     const bio = document.getElementById('bio');
     const github = document.getElementById('github');
@@ -40,10 +26,4 @@ switchTheme.addEventListener('click', function onClick(event) {
         linkedin.style.color = 'rgb(249, 250, 251)';
         email.style.color = 'rgb(249, 250, 251)';
     }
-    // IN DEVELOPMENT    
-    // if (localStorage.switchTheme === 'black' || (!('switchTheme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    //     document.documentElement.classList.add('black')
-    // } else {
-    //     document.documentElement.classList.remove('black')
-    // }
 });
